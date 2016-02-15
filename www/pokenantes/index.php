b09ebc926fd363bcc112130c3f75a1bdda985d11
Vincent MEZACHE <mezvin@gmail.com>
2016-02-15 13:08:37 +0100

    Upload du www complet

---
+++ www/pokenantes/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Bienvenue</title>
+		<meta charset="utf-8" />
+		<link href="view/css/bootstrap.css" rel="stylesheet">
+		<script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<header></header>
+		<main>
+			<nav>
+				<div>
+					<img src="view/img/logo.png" alt="logo"/>
+				</div>
+				<div>
+				    <ul>
+					  	<li><a href="#">Accueil</a></li>
+					    <li><a href="#">Ajouter produit</a></li>
+					    <li><a href="#">Catégories produit</a></li>
+				    </ul>
+				 </div>
+				 <div>
+				 	Nom Utilisateur
+				 </div>
+				 <div>
+				 	<a href="#">Déconnexion</a>
+				 </div>
+			</nav>
+
+			<aside>
+				<section>
+					<div class="box">
+						<img src="box.png" alt="AJOUTER UN PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+					<div class="box">
+						<img src="box.png" alt="CATEGORIE PRODUIT"/>
+					</div>
+				</section>
+			<aside>
+		</main>
+		<footer>
+			<h5>crédit : imie 2016 POEC PHP</h5>
+		</footer>
+
+	</body>
+</html>
---
+++ www/pokenantes/view/header.php
@@ -0,0 +1,20 @@
+<!-- // header -->
+
+<nav>
+	<div>
+		<img src="view/img/logo.png" alt="logo"/>
+	</div>
+	<div>
+	    <ul>
+		  	<li><a href="#">Accueil</a></li>
+		    <li><a href="#">Ajouter produit</a></li>
+		    <li><a href="#">Catégories produit</a></li>
+	    </ul>
+	 </div>
+	 <div>
+	 	Nom Utilisateur
+	 </div>
+	 <div>
+	 	<a href="#">Déconnexion</a>
+	 </div>
+</nav>
