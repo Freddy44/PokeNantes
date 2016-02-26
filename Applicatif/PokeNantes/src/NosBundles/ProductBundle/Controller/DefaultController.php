ba05b498bfdc4477c3e34e476b7f244b6710ad0f
OllyBilly <marine.billard.tic@gmail.com>
2016-02-26 12:17:45 +0100

    Front-Twig-Implémenté

--- Applicatif/PokeNantes/src/NosBundles/ProductBundle/Controller/DefaultController.php
+++ Applicatif/PokeNantes/src/NosBundles/ProductBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('NosBundlesProductBundle:Default:index.html.twig');
+        return $this->render('NosBundlesProductBundle:Template:index.html.twig');
     }
 }
